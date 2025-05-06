5ad6106a41a54794ab5b97fe0413ecbc2c75f8b4
Julian Santiago Diaz <107156927+juliansdiaz@users.noreply.github.com>
2025-05-06 15:52:20 -0500

    Deleted repeated variables, and created Mokepon class and objects

--- js/main.js
+++ js/main.js
@@ -1,42 +1,56 @@
+const selectBtn = document.getElementById("select-button");
+const fireBtn = document.getElementById("fire-attack-button");
+const waterBtn = document.getElementById("water-attack-button");
+const dirtBtn = document.getElementById("dirt-attack-button");
+const restartBtn = document.getElementById("restart-button");
+const selectAttackSection = document.getElementById("select-attack");
+const restartSection = document.getElementById("restart");
+const selectPetSection = document.getElementById("select-pet");
+const hipodogeInput = document.getElementById("hipodoge");
+const capipepoInput = document.getElementById("capipepo");
+const ratigueyaInput = document.getElementById("ratigueya");
+const playerPetSpan = document.getElementById("player-pet");
+const enemyPetSpan = document.getElementById("enemy-pet");
+const playerLives = document.getElementById("player-lives");
+const enemyLives = document.getElementById("enemy-lives");
+const messagesSection = document.getElementById("combat-result");
+const playerAttackMessage = document.getElementById("player-attack-message");
+const enemyAttackMessage = document.getElementById("enemy-attack-message");
+
+let mokepons = [];
 let playerAttack;
 let enemyAttack;
 let playerLivesCount = 3;
 let enemyLivesCount = 3;
 
+class Mokepon {
+  constructor(name, photo, lives) {
+    this.name = name;
+    this.photo = photo;
+    this.lives = lives;
+  }
+}
+
+let hipodoge = new Mokepon("Hipodoge", "./img/mokepons_mokepon_hipodoge_attack.png", 5);
+let capipepo = new Mokepon("Capipepo", "./img/mokepons_mokepon_capipepo_attack.png", 5);
+let ratigueya = new Mokepon("Ratigueya", "./img/mokepons_mokepon_ratigueya_attack.png", 5);
+
+mokepons.push(hipodoge, capipepo, ratigueya);
+
 function StartGame() {
-  let selectBtn = document.getElementById("select-button");
   selectBtn.addEventListener("click", SelectPlayerPet);
-
-  let fireBtn = document.getElementById("fire-attack-button");
   fireBtn.addEventListener("click", FireAttack);
-
-  let waterBtn = document.getElementById("water-attack-button");
   waterBtn.addEventListener("click", WaterAttack);
-
-  let dirtBtn = document.getElementById("dirt-attack-button");
   dirtBtn.addEventListener("click", DirtAttack);
-
-  let restartBtn = document.getElementById("restart-button");
   restartBtn.addEventListener("click", RestartGame);
 
-  let selectAttackSection = document.getElementById("select-attack");
   selectAttackSection.style.display = "none";
-
-  let restartSection = document.getElementById("restart");
   restartSection.style.display = "none";
 }
 
 function SelectPlayerPet() {
-  let selectAttackSection = document.getElementById("select-attack");
   selectAttackSection.style.display = "flex";
-
-  let selectPetSection = document.getElementById("select-pet");
   selectPetSection.style.display = "none";
-
-  let hipodogeInput = document.getElementById("hipodoge");
-  let capipepoInput = document.getElementById("capipepo");
-  let ratigueyaInput = document.getElementById("ratigueya");
-  let playerPetSpan = document.getElementById("player-pet");
 
   if (hipodogeInput.checked) {
     playerPetSpan.innerHTML = "Hipodoge";
@@ -57,7 +71,6 @@ function SelectPlayerPet() {
 
 function SelectEnemyPet() {
   let selectedPet = GenerateRandomNumber(1, 3);
-  let enemyPetSpan = document.getElementById("enemy-pet");
 
   if (selectedPet === 1) {
     enemyPetSpan.innerHTML = "Hipodoge";
@@ -102,9 +115,6 @@ function RandomEnemyAttack() {
 }
 
 function Combat() {
-  let playerLives = document.getElementById("player-lives");
-  let enemyLives = document.getElementById("enemy-lives");
-
   if (enemyAttack == playerAttack) {
     CreateBattleMessage("EMPATE");
   }
@@ -132,10 +142,6 @@ function CheckLives() {
 }
 
 function CreateBattleMessage(battleResult) {
-  let messagesSection = document.getElementById("combat-result");
-  let playerAttackMessage = document.getElementById("player-attack-message");
-  let enemyAttackMessage = document.getElementById("enemy-attack-message");
-
   let newPlayerBattleMessage = document.createElement("p");
   let newEnemyBattleMessage = document.createElement("p");
 
@@ -144,24 +150,16 @@ function CreateBattleMessage(battleResult) {
   newEnemyBattleMessage.innerHTML = enemyAttack;
 
   playerAttackMessage.appendChild(newPlayerBattleMessage);
-  enemyAttackMessage.appendChild( newEnemyBattleMessage);
+  enemyAttackMessage.appendChild(newEnemyBattleMessage);
 }
 
 function CreateGameOverMessage(gameOverResult) {
-  let messagesSection = document.getElementById("combat-result");
-
   messagesSection.innerHTML = gameOverResult;
 
-  let fireBtn = document.getElementById("fire-attack-button");
   fireBtn.disabled = true;
-
-  let waterBtn = document.getElementById("water-attack-button");
   waterBtn.disabled = true;
-
-  let dirtBtn = document.getElementById("dirt-attack-button");
   dirtBtn.disabled = true
 
-  let restartSection = document.getElementById("restart");
   restartSection.style.display = "block";
 }
 
